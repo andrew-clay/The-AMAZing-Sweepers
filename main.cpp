20b96f3357de646c5f3f956cd3bdafe0a71fbb32
andrew-clay <155792643+andrew-clay@users.noreply.github.com>
2024-06-13 16:08:51 -0500

    Create main.cpp
    Zaina William's original commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>  // Needed for random in flavorText function.
+#include <time.h>   // Needed for a more random random.
+#include "printFunctions.h"
+
+int main(){
+    srand(time(0)); // Needed for random in flavorText function.
+
+
+    return 0;
+}
+

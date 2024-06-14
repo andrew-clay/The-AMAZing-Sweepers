8e9f201e9efec4d27d162a7ae2d948739c1a8507
Thomas Hammond <138634903+TDHammond@users.noreply.github.com>
2024-06-13 23:31:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <cstdlib>  // Needed for random in flavorText function.
 #include <time.h>   // Needed for a more random random.
 #include "printFunctions.h"
+#include "mazeGeneration.h"
+#include "mapFunctions.h"
+
+struct Player{
+    int x = 1;
+    int y = 1;
+};
 
 int main(){
     srand(time(0)); // Needed for random in flavorText function.
+    Player player;
+    int width = 30;
+    int height = 15;
+    Maze mazeGenerator(width, height);
+    MapManager mapManager(width, height);
+    mapManager.DEBUG_SetMaze(mazeGenerator);
 
+    mazeGenerator.printMaze();
 
+    mapManager.DEBUG_PrintValidMovementOptions(player.x, player.y);
+
+    string playerFacing = "East";
+    vector<string> adjustedOptions;
+
+    adjustDirection(mapManager.validMovementOptions(player.x,player.y), adjustedOptions, playerFacing);
+
+    printOptions(adjustedOptions);
     return 0;
 }
 
